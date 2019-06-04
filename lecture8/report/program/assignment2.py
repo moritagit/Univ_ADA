9c49e4edff8283f196af5d26aae63156633eaa8e
moritagit <ryousuke654@gmail.com>
2019-06-04 22:21:47 +0900

    finished lecture8 reports

--- lecture8/report/program/assignment2.py
+++ lecture8/report/program/assignment2.py
@@ -21,16 +21,25 @@ def phi(x):
 
 
 def update(x, y, gamma, theta):
+    #import pdb; pdb.set_trace()
     mu, sigma = theta
+    n_sample = x.shape[0]
     phi_x = phi(x)
 
     beta = gamma + (phi_x.dot(sigma) * phi_x).sum(axis=1)
 
-    tmp = 1 - phi_x.dot(mu) * y
-    tmp[tmp < 0] = 0
-    d_mu = (y * tmp / beta) * sigma.dot(phi_x.T).T
+    hinge = 1 - phi_x.dot(mu) * y
+    hinge[hinge < 0] = 0
+    d_mu = (y * hinge / beta)[:, np.newaxis] * sigma.dot(phi_x.T).T
+    d_mu = d_mu.mean(axis=0)
 
-    d_sigma = sigma.dot(phi_x).dot(phi_x.T).dot(sigma) / beta
+    d_sigma = np.zeros_like(sigma)
+    for i in range(n_sample):
+        _phi_x = phi_x[i, :]
+        _beta = beta[i]
+        tmp = sigma.dot(_phi_x)[:, np.newaxis]
+        d_sigma += tmp.dot(tmp.T) / _beta
+    d_sigma /= n_sample
 
     mu_new = mu + d_mu
     sigma_new = sigma - d_sigma
@@ -48,13 +57,18 @@ def compute_loss(x, y, gamma, theta, theta_old):
     loss_main = 1 - phi_x.dot(mu) * y
     loss_main[loss_main < 0] = 0
     loss_main = loss_main ** 2
-    loss_main = loss_main.mean(axis=0)
+    loss_main = loss_main.mean()
 
     loss_var = (phi_x.dot(sigma) * phi_x).sum(axis=1)
-    loss_main.mean(axis=0)
+    loss_var = loss_var.mean()
 
-    loss_KL = np.log(np.linalg.det(sigma_old) / np.linalg.det(sigma)) + np.trace(sigma_old_inv.dot(sigma)) + (mu - mu_old).T.dot(sigma_old_inv).dot(mu - mu_old) - d
-    loss_KL.mean(axis=0)
+    loss_KL = (0
+        + np.log(np.linalg.det(sigma_old) / np.linalg.det(sigma))
+        + np.trace(sigma_old_inv.dot(sigma))
+        + (mu - mu_old).T.dot(sigma_old_inv).dot(mu - mu_old)
+        - d
+        )
+    loss_KL.mean()
 
     loss = loss_main + loss_var + gamma * loss_KL
     return loss, loss_main, loss_var, loss_KL
@@ -63,11 +77,11 @@ def compute_loss(x, y, gamma, theta, theta_old):
 def train(x, y, gamma, epochs=1, batch_size=1, shuffle=True):
     d = x.shape[1]
     n_sample = x.shape[0]
-    mu = np.zeros(d)
-    sigma = np.zeros((d, d))
+    mu = np.random.random(d)
+    sigma = np.diag(np.random.random(d) + 0.1)
     theta = (mu, sigma)
-    for epoch in epochs:
-        print(f'Epoch: {epoch}')
+    print('train')
+    for epoch in range(1, 1+epochs):
         if shuffle:
             idx = np.random.permutation(np.arange(n_sample))
             x = x[idx]
@@ -77,7 +91,7 @@ def train(x, y, gamma, epochs=1, batch_size=1, shuffle=True):
             x_mini = x[i:i+batch_size]
             y_mini = y[i:i+batch_size]
 
-            theta_new = update(x, y, gamma, theta)
+            theta_new = update(x_mini, y_mini, gamma, theta)
 
             losses = compute_loss(x_mini, y_mini, gamma, theta_new, theta)
             loss_list.append(losses)
@@ -86,8 +100,8 @@ def train(x, y, gamma, epochs=1, batch_size=1, shuffle=True):
 
         losses = np.array(loss_list).mean(axis=0)
         loss, loss_main, loss_var, loss_KL = tuple(losses)
-        print('\tLoss: {loss:.4f}  main: {loss_main:.4f}  var: {loss_var:.4f}  KL: {loss_KL:.4f}')
-
+        print(f'Epoch: {epoch}  Loss: {loss:.4f}  (main: {loss_main:.4f}  var: {loss_var:.4f}  KL: {loss_KL:.4f})')
+    print()
     return theta
 
 
@@ -96,14 +110,24 @@ def visualize(x, y, theta, num=100, offset=1.0, path=None):
     x2_max, x2_min = x[:, 1].max(), x[:, 1].min()
 
     X = np.linspace(x1_min, x1_max, num=num)
+    mu, sigma = theta
 
-
-    plt.clf()
     plt.xlim(x1_min-offset, x1_max+offset)
-    plt.xlim(x2_min-offset, x2_max+offset)
+    plt.ylim(x2_min-offset, x2_max+offset)
 
     plt.scatter(x[(y==1), 0], x[(y==1), 1] , c='blue', marker='o')
     plt.scatter(x[(y==-1), 0], x[(y==-1), 1] , c='red', marker='x')
+
+    if abs(mu[0]) > abs(mu[1]):
+        plt.plot(
+            [x1_min, x1_max],
+            [-(mu[2]+mu[0]*x1_min)/mu[1], -(mu[2]+mu[0]*x1_max)/mu[1]]
+            )
+    else:
+        plt.plot(
+            [-(mu[2]+mu[1]*x2_min)/mu[0], -(mu[2]+mu[1]*x2_max)/mu[0]],
+            [x2_min, x2_max]
+            )
 
     if path:
         plt.savefig(path)
@@ -112,10 +136,10 @@ def visualize(x, y, theta, num=100, offset=1.0, path=None):
 
 def main():
     # settings
-    gamma = 0.1
+    gamma = 1.0
     n_sample = 50
-    batch_size = 5
-    epochs = 5
+    batch_size = 10
+    epochs = 50
     fig_path = '../figures/assignment2_result.png'
     np.random.seed(0)
 
@@ -126,11 +150,15 @@ def main():
 
     # train
     theta = train(x, y, gamma, epochs=epochs, batch_size=batch_size)
+    mu, sigma = theta
 
     # result
+    print('result')
     print(f'#Sample: {n_sample}')
     print(f'gamma: {gamma}')
-    print(f'theta: \n{theta}')
+    print(f'mu: {mu}')
+    print(f'sigma: \n{sigma}')
+
     visualize(x, y, theta, path=fig_path)
 
 
